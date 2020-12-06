b795c25f598a38c4f93bbc4a1984d2259cf40921
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2020-12-06 21:50:48 +0100

    BLD-61 missing equals()/hashCode() in PluginDownload

--- buildsystem-maven-core/src/main/java/com/neaterbits/build/buildsystem/maven/targets/PluginDownload.java
+++ buildsystem-maven-core/src/main/java/com/neaterbits/build/buildsystem/maven/targets/PluginDownload.java
@@ -33,6 +33,31 @@ final class PluginDownload {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((plugin == null) ? 0 : plugin.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final PluginDownload other = (PluginDownload) obj;
+        if (plugin == null) {
+            if (other.plugin != null)
+                return false;
+        } else if (!plugin.equals(other.plugin))
+            return false;
+        return true;
+    }
+
+    @Override
     public String toString() {
         return "PluginDownload [plugin=" + plugin.getModuleId().getId() + ", repositories=" + repositories + "]";
     }
