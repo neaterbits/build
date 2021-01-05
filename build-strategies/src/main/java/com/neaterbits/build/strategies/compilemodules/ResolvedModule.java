bf51fb2dd092167813e2de8afb273ef2113ef506
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2021-01-05 08:29:11 +0100

    COM-43 refactor test builders

--- build-strategies/src/main/java/com/neaterbits/build/strategies/compilemodules/ResolvedModule.java
+++ build-strategies/src/main/java/com/neaterbits/build/strategies/compilemodules/ResolvedModule.java
@@ -3,38 +3,24 @@ package com.neaterbits.build.strategies.compilemodules;
 import java.util.List;
 import java.util.Objects;
 
-import com.neaterbits.util.coll.Coll;
-
-public final class ResolvedModule<PARSED_FILE, RESOLVE_ERROR> {
+public final class ResolvedModule<PARSED_FILE, RESOLVE_ERROR> 
+    extends BaseResolvedModule<PARSED_FILE, RESOLVE_ERROR> {
 
     private final CompileModule compileModule;
-    private final ParsedModule<PARSED_FILE, RESOLVE_ERROR> parsedModule;
-    private final List<RESOLVE_ERROR> resolveErrors;
     
     ResolvedModule(
             CompileModule compileModule,
             ParsedModule<PARSED_FILE, RESOLVE_ERROR> parsedModule,
             List<RESOLVE_ERROR> resolveErrors) {
         
+        super(parsedModule, resolveErrors);
+        
         Objects.requireNonNull(compileModule);
-        Objects.requireNonNull(parsedModule);
         
         this.compileModule = compileModule;
-        this.parsedModule = parsedModule;
-        this.resolveErrors = resolveErrors != null
-                    ? Coll.immutable(resolveErrors)
-                    : null;
     }
 
     public CompileModule getCompileModule() {
         return compileModule;
-    }
-
-    public ParsedModule<PARSED_FILE, RESOLVE_ERROR> getParsedModule() {
-        return parsedModule;
-    }
-
-    public List<RESOLVE_ERROR> getResolveErrors() {
-        return resolveErrors;
     }
 }
